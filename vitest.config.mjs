a2257378ef23db63beec8382387349b49821b7f6
FlechaMaker <flechacreador@gmail.com>
2024-11-12 12:13:33 +0900

    vitest.config.mjsの設定を更新し、テスト環境用の環境変数を追加。パスエイリアスを設定。

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,10 +1,19 @@
+import path from "path";
+
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
+import dotenv from "dotenv";
 
 export default defineConfig({
   plugins: [tsConfigPaths(), react()],
   test: {
     environment: "jsdom",
+    env: dotenv.config({ path: ".env.test.local" }).parsed,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
   },
 });
